1c43db7d3ccf013ac25b20ebf77570996b303bb4
Pavan Teja <konavenkatasaipa.teja_ext@novartis.com>
2023-02-22 11:13:50 +0530

    Initial Commit

---
+++ On The Map/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  On The Map
+//
+//  Created by TEJAKO3-Old Mac on 22/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
